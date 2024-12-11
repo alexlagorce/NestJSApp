aaf042e507ded6a5b9e395976c8fd3c8faddb450
Alexandre Lagorce <alexandrelagorce@MacBook-Pro-de-Alexandre.local>
2024-12-11 11:53:00 +0100

    add qs

--- src/instagram/instagram.service.ts
+++ src/instagram/instagram.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { ConfigService } from '@nestjs/config';
+import qs from 'qs'; // Utilisez qs pour convertir l'objet en chaîne de requête
 
 @Injectable()
 export class InstagramService {
@@ -12,15 +13,19 @@ export class InstagramService {
         this.accessToken = this.configService.get<string>('INSTAGRAM_ACCESS_TOKEN');
       }
 
-    async exchangeCodeForToken(code: string) {
+      async exchangeCodeForToken(code: string) {
         try {
-            const response = await axios.post('https://api.instagram.com/oauth/access_token', {
-                client_id: '8810392132361238',
-                client_secret: '6f3355913a763664e69',
-                grant_type: 'authorization_code',
-                redirect_uri: 'https://nestjsapp.onrender.com/instagram/callback',
-                code,
-            });
+            const response = await axios.post(
+                'https://api.instagram.com/oauth/access_token',
+                qs.stringify({ // Utilisez qs.stringify ici
+                    client_id: '8810392132361238',
+                    client_secret: '6f3355913a763664e69',
+                    grant_type: 'authorization_code',
+                    redirect_uri: 'https://nestjsapp.onrender.com/instagram/callback',
+                    code,
+                }),
+                { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } } // Ajoutez le bon header
+            );
             return response.data;
         } catch (error) {
             console.error('Error exchanging code for token:', error.response?.data || error.message);
